ddcc33f5f86e79e71109af6699733a6bde735ad5
Lee In Seok <dlstjr9068@gmail.com>
2022-10-25 16:01:49 +0900

    RestTemplate Response Type 예제 지정

--- src/main/java/kr/co/inseok/D_20221030/QnA.java
+++ src/main/java/kr/co/inseok/D_20221030/QnA.java
@@ -5,6 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import kr.co.inseok.D_20221030.domain.GenericVO;
 import kr.co.inseok.D_20221030.domain.Person;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 //슈퍼 타입 토큰에 대한 개념과 간단한 예시
 public class QnA {
@@ -26,5 +32,16 @@ public class QnA {
 
         System.out.println(person);
         System.out.println(genericPerson);
+
+        ResponseEntity<Person> responseEntity1 = new RestTemplate().exchange("https://www.test.com",
+                HttpMethod.GET,
+                new HttpEntity<>(new HttpHeaders()),
+                Person.class);
+
+        ResponseEntity<GenericVO<Person>> responseEntity2 = new RestTemplate().exchange("https://www.test.com",
+                HttpMethod.GET,
+                new HttpEntity<>(new HttpHeaders()),
+                new ParameterizedTypeReference<GenericVO<Person>>() {
+                });
     }
 }
